8eecc6bb0b9d7271a9b71e5db1997020689e37dc
Daryl Abellera <abelleradaryl@gmail.com>
2022-06-08 19:51:51 -0700

    Finished assignment

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ Use the copy function below to do the following:
 */
 
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(flavors){
+  return [...flavors];
 }
 
 
@@ -63,8 +63,8 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-  /*your code here*/
+function is31Flavors(count){
+  return (count.length === originalFlavors.length);
  }
 
 
@@ -82,8 +82,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-  /*your code here*/
+function addFlavor(originalFlavors, newFlav){
+  originalFlavors.unshift(newFlav);
+  return originalFlavors;
  }
 
 
@@ -100,8 +101,9 @@ Use the removeLastFlavor function below to do the following:
 */
 
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(flavors){
+ flavors.pop();
+ return flavors;
 }
 
 
@@ -118,8 +120,8 @@ Use the getFlavorByIndex function below to do the following:
 */
 
 
-function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
+function getFlavorByIndex(newFlav, num){
+ return newFlav[num];
 }
 
 
@@ -138,8 +140,9 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-  /*your code here*/
+function removeFlavorByName(flavors, name){
+  flavors.splice(flavors.indexOf(name), 1);
+  return flavors;
 }
 
 
@@ -163,8 +166,14 @@ Use the filterByWord function below to do the following:
 */
 
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(flavors, name){
+  const newArr = [];
+  for (let i = 0; i < flavors.length; i++){
+    if (flavors[i].includes(name)) {
+    newArr.push(flavors[i]);
+  }
+}
+  return newArr;
 }
 
 
